28d244622a16f7f94d7c46dffa14da91d9019e36
Michael Shire <mike_shire@hotmail.com>
2021-10-05 17:36:53 -0400

    setting file to index.html

--- build/startup.sh
+++ build/startup.sh
@@ -4,4 +4,16 @@ oc login --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` --ins
 # set the now variable to the current date
 now=$(date +"%m_%d_%Y")
 # run the python script with the TARGET_NAMESPACE variable and output to the expected PVC
-python ./report.py -n ${TARGET_NAMESPACE} -o /output/report-${now}.html
+python ./report.py -n ${TARGET_NAMESPACE} -o /output/index.html
+
+
+For the nginx container:
+
+          volumeMounts:
+            - name: report-out
+              mountPath: /usr/share/nginx/html
+              readOnly: true
+      volumes:
+        - name: report-out
+          persistentVolumeClaim:
+            claimName: output
