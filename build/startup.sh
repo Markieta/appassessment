8de061157f3e2e556606857ba39ee6ab420e2299
Michael Shire <mike_shire@hotmail.com>
2021-10-05 15:58:18 -0400

    fourth attempt

--- build/startup.sh
+++ build/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # login to the oc cli using the pod's serviceaccount token
-oc login --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` --server=https://kubernetes.default.svc
+oc login --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` --insecure-skip-tls-verify=true --server=https://kubernetes.default.svc
 # set the now variable to the current date
 now=$(date +"%m_%d_%Y")
 # run the python script with the TARGET_NAMESPACE variable and output to the expected PVC
